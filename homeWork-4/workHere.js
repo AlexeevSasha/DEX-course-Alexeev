86fd155a35d23c0c8fccce3ea524530ebf3dfd7d
AlexeevSasha <work.alexeevs@gmail.com>
2021-11-18 12:09:19 +0200

    homeWork

---
+++ homeWork-4/workHere.js
@@ -0,0 +1,33 @@
+//TODO: Напишите функцию счётчик вызовов, должна принимать название и функцию, а возвращать функцию
+// Выводит в консоль, в момент вызова функции из параметров, кол-во вызовов
+export const counter = (name, func) => {
+    let count = 0;
+    return function () {
+        console.log(`${name}:`, ++count);
+        return func.call(this);
+    };
+};
+
+//TODO: Напишите функцию логгер, должна принимать название и функцию, а возвращать функцию
+// Выводит в консоль, в момент вызова функции из параметров, её параметры результат и название
+export const logger = (name, func) => {
+    return function (...args) {
+        console.log(`Парметры ${name}:`, ...args);
+        return func.call(this);
+    };
+};
+
+//TODO: Напишите функцию каррирования, должна принимать функцию, а возвращать функцию
+// Каррирование – это трансформация функций таким образом, чтобы они принимали аргументы не как f(a, b, c), а как f(a)(b)(c)
+// Должна работать для любого кол-ва аргументов
+
+export const curry = (func) => {
+    return function funCurr(...arg) {
+        const curridFunc = func.bind(this);
+        if (func.length === arg.length) {
+            return curridFunc(...arg);
+        } else {
+            return (...arg2) => funCurr.call(this, ...arg, ...arg2);
+        }
+    };
+};
